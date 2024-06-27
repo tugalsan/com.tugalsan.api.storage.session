5a448d04d6ecb531ea09f7666f68ec2bf23f2030
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:04:28 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module com.tugalsan.api.storage.session {
     requires gwt.user;
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     requires com.tugalsan.api.thread;
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.list;
